44bd5882c89a9f153c1e81651a22fa558fb99bfe
krzyszto_gajda <gajda98@wp.pl>
2022-12-19 20:01:06 +0100

    branch creation, fragment files name changes

--- app/src/main/java/com/example/motionnotes/CheckListsFragment.java
+++ app/src/main/java/com/example/motionnotes/CheckListsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CheckListFragment#newInstance} factory method to
+ * Use the {@link CheckListsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CheckListFragment extends Fragment {
+public class CheckListsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class CheckListFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CheckListFragment() {
+    public CheckListsFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class CheckListFragment extends Fragment {
      * @return A new instance of fragment CheckListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CheckListFragment newInstance(String param1, String param2) {
-        CheckListFragment fragment = new CheckListFragment();
+    public static CheckListsFragment newInstance(String param1, String param2) {
+        CheckListsFragment fragment = new CheckListsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class CheckListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_list, container, false);
+        return inflater.inflate(R.layout.fragment_check_lists, container, false);
     }
 }
