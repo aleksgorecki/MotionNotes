0e216de6b1569fe731b08046f7831659b8c2a6ac
krzyszto_gajda <gajda98@wp.pl>
2022-12-20 15:18:38 +0100

    added UPDATE to all, added SELECT "one" to notes, event, checkList

--- app/src/main/java/com/example/motionnotes/DataBaseHelper.java
+++ app/src/main/java/com/example/motionnotes/DataBaseHelper.java
@@ -74,9 +74,8 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public List<Note> getAllNotes(){
         List<Note> returnList=new ArrayList<>();
 
-        String queryString="SELECT * FROM notes";
-
         SQLiteDatabase db=this.getReadableDatabase();
+        String queryString="SELECT * FROM notes";
 
         Cursor result = db.rawQuery(queryString,null);
 
@@ -96,6 +95,43 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         result.close();
         db.close();
         return returnList;
+    }
+
+    public Note getNote(int id){
+        SQLiteDatabase db=this.getReadableDatabase();
+        String queryString="SELECT FROM notes WHERE id = "+id;
+
+        Cursor result=db.rawQuery(queryString,null);
+
+        if(result.moveToFirst()){
+            String content=result.getString(1);
+            int position=result.getInt(2);
+
+            result.close();
+            db.close();
+
+            return new Note(id, content, position);
+        }
+        else {
+            result.close();
+            db.close();
+            return null;
+        }
+    }
+
+    public boolean updateNote(Note note){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+
+        cv.put("content", note.getContent());
+        cv.put("position",note.getPosition());
+
+        int update=db.update("notes",cv,"id ?",new String[]{Integer.toString(note.getId())});
+        if(update>0){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean deleteNote(Note note){
@@ -162,6 +198,46 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         return returnList;
     }
 
+    public Event getEvent(int id){
+        SQLiteDatabase db=this.getReadableDatabase();
+        String queryString="SELECT FROM events WHERE id = "+id;
+
+        Cursor result=db.rawQuery(queryString,null);
+
+        if(result.moveToFirst()){
+            do {
+                String name=result.getString(1);
+                String date=result.getString(2);
+                String hour=result.getString(3);
+                String content=result.getString(4);
+                int position=result.getInt(5);
+
+                return new Event(id, name,date, hour, content, position);
+            } while (result.moveToNext());
+        }
+        else{
+            return null;
+        }
+    }
+
+    public boolean updateEvent(Event event){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+
+        cv.put("name",event.getName());
+        cv.put("date", event.getDate());
+        cv.put("hour", event.getHour());
+        cv.put("content", event.getContent());
+        cv.put("position", event.getPosition());
+
+        int update=db.update("events",cv,"id ?",new String[]{Integer.toString(event.getId())});
+        if(update>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public boolean deleteEvent(Event event){
         SQLiteDatabase db=this.getWritableDatabase();
         String queryString="DELETE FROM events WHERE id = "+event.getId();
@@ -220,17 +296,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         return returnList;
     }
 
-    public CheckList getCheckList(String name){
-        CheckList checkList;
-
-        String queryString="SELECT FROM lists WHERE name LIKE "+name;
+    public CheckList getCheckList(int id){
 
         SQLiteDatabase db=this.getReadableDatabase();
+        String queryString="SELECT FROM lists WHERE id = "+id;
 
         Cursor result = db.rawQuery(queryString,null);
 
         if(result.moveToFirst()){
-            int id=result.getInt(0);
             String r_name=result.getString(1);
             int position=result.getInt(2);
 
@@ -243,6 +316,44 @@ public class DataBaseHelper extends SQLiteOpenHelper {
             result.close();
             db.close();
             return null;
+        }
+    }
+
+    public CheckList getCheckList(String name){
+
+        SQLiteDatabase db=this.getReadableDatabase();
+        String queryString="SELECT FROM lists WHERE name LIKE "+name;
+
+        Cursor result = db.rawQuery(queryString,null);
+
+        if(result.moveToFirst()){
+            int id=result.getInt(0);
+            String r_name=result.getString(1);
+            int position=result.getInt(2);
+
+            result.close();
+            db.close();
+            return new CheckList(id,r_name,position);
+        }
+        else {
+            result.close();
+            db.close();
+            return null;
+        }
+    }
+
+    public boolean updateCheckList(CheckList checkList){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+
+        cv.put("name", checkList.getName());
+        cv.put("position", checkList.getPosition());
+
+        int update=db.update("lists",cv,"id ?",new String[]{Integer.toString(checkList.getId())});
+        if(update>0){
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -306,6 +417,23 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         result.close();
         db.close();
         return returnList;
+    }
+
+    public boolean updateItem(Item item){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+
+        cv.put("content", item.getContent());
+        cv.put("is_done", item.isDone());
+        cv.put("position", item.getPosition());
+        cv.put("list_id", item.getList_id());
+
+        int update=db.update("items",cv,"id ?",new String[]{Integer.toString(item.getId())});
+        if(update>0){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean deleteItem(Item item){
